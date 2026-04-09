54775f5c5d05d98305be23a2139a371ef20687c1
nosekp <glo3025pando@gmail.com>
2026-04-09 16:10:54 -0300

    a little for twwiter

--- main.js
+++ main.js
@@ -1,54 +1,61 @@
-import puppeteer from 'puppeteer';
-import { supabase } from './supabaseclient.js';
+import puppeteer from "puppeteer";
 
-(async () => {
-  const URL = "https://x.com/5ro4";
+await scrap("5ro4");
+
+async function scrap(user) {
+  const URL = `https://x.com/${user}`;
 
   const browser = await puppeteer.launch({
-    headless: false
+    headless: false,
   });
 
   const page = await browser.newPage();
   await page.goto(URL, { waitUntil: "networkidle2" });
-
-  const title = await page.title();
-  console.log(`Titulo de la pagina: ${title}`);
-
-  // Esperar a que carguen los tweets
-  await page.waitForSelector('article[data-testid="tweet"]', { timeout: 10000 });
-
-  // Scroll para cargar contenido
-  await page.evaluate(() => window.scrollTo(0, 2000));
-
-  for (let i = 0; i < 3; i++) {
-    await page.evaluate(() => window.scrollTo(0, 2000));
-  }
-
-  const post = await page.evaluate(() => {
-    const articles = Array.from(
-      document.querySelectorAll('article[data-testid="tweet"]')
-    );
-
-    //  (implicit return)
-    return articles.map(article => ({
-      text: article.querySelector('div[data-testid="tweetText"]')?.innerText || 'Sin texto',
-      images: Array.from(article.querySelectorAll('div[data-testid="tweetPhoto"] img'))
-        .map(img => img.src),
-      videos: Array.from(article.querySelectorAll('div[data-testid="videoComponent"] img'))
-        .map(img => img.src)
-    }));
+  await page.setViewport({
+    width: 1200,
+    height: 800,
   });
 
-  console.log('Total de posts:', post.length);
-  console.log(post);
+  await autoScroll(page);
+
+  await scrapeSomething(page);
 
   await browser.close();
-})();
+}
 
+async function scrapeSomething(page) {
+  const data = await page.evaluate(() => {
+    return Array.from(
+      document.querySelectorAll('div[data-testid="tweetText"]'),
+      (e) => ({
+        postTetx: e.innerText,
+      }),
+    );
+  });
 
+  console.log("This is Data: ", data);
+}
+
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve) => {
+            let totalHeight = 0;
+            let distance = 100;
+            let timer = setInterval(() => {
+                let scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight - window.innerHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
+
+// const title = await page.title();
+// console.log(`Titulo de la pagina: ${title}`);
 
 // document.querySelectorAll('div[data-testid="tweetText"').innerText
-
-/*
-Intercepta la respuesta de el video y converti la playlist en un video con fmpeg
-*/
